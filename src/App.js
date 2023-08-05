0326a337ffd197dcb45b8636574581a466b8eae0
howkat23 <howardkaterina20@gmail.com>
2023-08-05 18:43:58 -0400

    your commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <h1>A Breed Show</h1>
+  </div>
+  const [dog, setDog] = useState(0);
+  const {id, name, description, breed, image} = data[dog];
+  const [showMore, setShowMore] = useState(false);
+
+  const previousDog = () => {
+    setDog((dog => {
+      dog --;
+      if (dog < 0) {
+        return data.length - 1;
+      }
+      return dog;
+    }))
+  }
+
+  const nextDog = () => {
+    setDog((dog => {
+      dog ++;
+      if (dog > data.length - 1) {
+        dog = 0;
+      }
+      return dog;
+    }))
+  }
+
+return (<div>
+
+  <div className="container">
+    <img src={image} width="300px" alt="dogs"/>
+  </div>
+
+  <div className="container">
+  <h2>{breed} (Dog's Breed)</h2>
+
+</div>
+
+
+<div className="container">
+  <h1>{id} - {name}</h1>
+</div>
+
+
+  <div className='container'>
+  <p>{showMore ? description : description.substring(0,220) + "...."}
+          <button onClick={() => setShowMore(!showMore)}>{showMore ? "Show less" : "Show more"}</button>
+          </p>
+</div>
+
+<div className="btn container">
+  <button onClick={previousDog}>Previous</button>
+  <button onClick={nextDog}>Next</button>
+
+</div>
+</div>)   
+
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const data = [
+    {
+    id: 1,
+    name: "Ace (on the left) & Alba (on the right)",
+    description: "Ace is often a moniker for someone who's an expert at what they do. If your pup is a pro at something–like catching a Frisbee–give them this cute dog name! If you ever studied Latin, you’ll know that this word means “white,” as well as being actress Jessica Alba’s surname. Law enforcement's top dog is a great family dog with proper training",
+    breed: "German Shepherd",
+    image: "https://cdn.pixabay.com/photo/2019/07/23/13/51/shepherd-dog-4357790_640.jpg"
+    },
+
+    {
+    id: 2,
+    name: "Duke",
+    description: "Just one of the many dog name ideas that we can steal from the characters on bridgerton. The Labrador Retriever has been the nation’s favorite dog for three decades and it’s not hard to see why. Labs are known for being incredibly loving, loyal and family-friendly. Energetic and outgoing, this popular pup lives for time spent in the Great Outdoors, so much so that they might even be part fish! If your weekend plans often include activities like hiking and swimming, you’ll find a willing and eager partner in the Lab. Their peppy personality and happy-go-lucky nature make for life’s sweetest adventures.",
+    breed: "Labrador",
+    image: "https://cdn.pixabay.com/photo/2016/11/26/23/45/dog-1861839_640.jpg"
+    },
+
+    {
+    id: 3,
+    name: "Gracie",
+    description: "If Olympic champion swimmer Michael Phelps were to assume canine form, he’d most likely be the Portuguese Water Dog. Originally bred to assist fishermen, these expert paddlers are affable hard workers who can get up to some mischief if not kept busy. No wonder they live their best lives as members of an active family—that’s why Bo and Sunny, a pair of Porties, fit right in when the Obamas welcomed them to the White House as “First Pets.” While there’s no requirement here to just add water (they are just fine in your dry land backyard), you will need to provide a lifestyle for a pup who’s up for more than just a walk around the block. Ready to dive into a relationship with a Portuguese Water Dog?",
+    breed: "Portuguese Water Dog",
+    image: "https://cdn.pixabay.com/photo/2016/01/29/20/54/dog-1168663_640.jpg"
+    },
+
+    {
+    id: 4,
+    name: "Jack",
+    description: "Make no mistake: The Doberman Pinscher is a fun-loving, family-oriented dog with an up-for-anything personality, whether it’s a run around the neighborhood or a lazy Sunday spent on the couch cuddling with their human. But these dogs also possess something else: a fierce loyalty and determination to protect their families when needed. And it’s precisely that bold bravery that makes them excel as guard dogs and police and military dogs. Fearless and assertive, Dobermans are known for their imposing, tough-dog exterior, but underneath you’ll discover they actually have a heart of gold.",
+    breed: "Doberman Pinscher",
+    image: "https://cdn.pixabay.com/photo/2016/04/17/10/38/doberman-1334497_640.jpg"
+    },
+
+    {
+    id: 5,
+    name: "Lila",
+    description: "Comes from the Arabic word for “night,” which is pretty perfect for a female pup whose fur is as black as night. You’ll definitely feel safer with a Cane Corso (KAH-neh KOR-so) around. This giant dog breed is fiercely loyal and protective of the people they love—able to intimidate strangers with a single look. Yet despite their imposing appearance, these big dogs can be very affectionate with their own family. Proper socialization and training are a must for this highly driven, smart-yet-stubborn breed, making the Cane Corso a good match for an experienced, assertive dog parent.",
+    breed: "Cane Corso",
+    image: "https://cdn.pixabay.com/photo/2018/11/01/00/55/animal-3786987_640.jpg"
+    }
+]
